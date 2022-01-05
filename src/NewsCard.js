7789aaffa4c5d74a261997232fa7a47991f02f77
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2022-01-04 20:09:59 -0700

    Add detailed, single-article view functionality

--- src/NewsCard.js
+++ src/NewsCard.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import './NewsCard.css'
 
-class NewsCard extends Component {
-  state = {
-    
-  }
+const NewsCard = ({ abstract, img, title, getArticleDetails }) => {
+
+  return (
+    <button
+      className="article-button"
+      onClick={ () => getArticleDetails(abstract, img.url) }
+    >
+      <img className="article-button__image" src={img.url} />
+      <h3>{ title }</h3>
+      <p>{ abstract }</p>
+    </button>
+  )
 }
 
 export default NewsCard
